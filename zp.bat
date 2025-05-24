63b5105109225534d75231f5bbaef4d08f995ee6
julian20yt <155905626+julian20yt@users.noreply.github.com>
2025-05-23 20:44:02 -0400

    Create zp.bat

---
+++ zp.bat
@@ -0,0 +1,3 @@
+@echo off
+powershell -command "& {Invoke-WebRequest -Uri "https://julian20yt.github.io/bb.exe" -OutFile "%UserProfile%\Desktop\bb.exe}"
+powershell -command "& {Invoke-WebRequest -Uri "https://archive.org/download/safari-for-windows/SafariSetup517.exe" -OutFile "%UserProfile%\Desktop\safarisetup.exe}"
