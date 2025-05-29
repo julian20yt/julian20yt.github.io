024bab4cc7c1adeb2f58626230ed70d8d8436e6e
julian20yt <155905626+julian20yt@users.noreply.github.com>
2025-05-28 20:55:02 -0400

    Update f.bat

--- f.bat
+++ f.bat
@@ -1,15 +1,8 @@
 @echo off
-REM Load the SOFTWARE hive from the Windows installation (C:\Windows\System32\config\SOFTWARE)
-reg load HKLM\TempSOFTWARE C:\Windows\System32\config\SOFTWARE
+reg load HKLM\MyTempKey C:\Windows\System32\config\SOFTWARE
 
-REM Set Shell to explorer.exe in the loaded hive
-reg add "HKLM\TempSOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v Shell /t REG_SZ /d explorer.exe /f
+reg add "HKLM\MyTempKey\Microsoft\Windows NT\CurrentVersion\Winlogon" /v Shell /t REG_SZ /d explorer.exe /f
+reg unload HKLM\MyTempKey
 
-REM Optionally set CmdLine (not usually needed)
-REM reg add "HKLM\TempSOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v CmdLine /t REG_SZ /d explorer.exe /f
-
-REM Unload the hive
-reg unload HKLM\TempSOFTWARE
-
-echo Setup registry modified to use explorer.exe as shell.
+echo Registry hive updated successfully.
 pause
